82254c034136f8ad7a74cdb361d09e373e324f01
kuni02q <70653128+kuni02q@users.noreply.github.com>
2026-09-06 10:38:47 +0200

    Add Watermelon (800)

---
+++ codeforces/800/4A - Watermelon.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Watermelon_4A {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int weight = sc.nextInt();
+
+
+
+
+        System.out.println(weight >2 && weight %2==0 ? "YES" : "NO");
+
+    }
+
+}
