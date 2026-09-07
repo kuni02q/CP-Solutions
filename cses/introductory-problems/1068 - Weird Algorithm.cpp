cbf15c7cebb0c220f528e450a81ab1c530c5cbeb
kuni02q <70653128+kuni02q@users.noreply.github.com>
2026-09-07 11:12:21 +0200

    Add Weird Algorithm (introductory-problems)

---
+++ cses/introductory-problems/1068 - Weird Algorithm.cpp
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class WeirdAlgorithm {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+
+        while (n != 1) {
+
+            System.out.print(n + " ");
+            n = n % 2 == 0 ? n / 2 : n * 3 + 1;
+
+        }
+
+        System.out.print(1);
+    }
+}
