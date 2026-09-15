9af47d2784b8277c3ae0d96edd76f954bb3614a3
kuni02q <70653128+kuni02q@users.noreply.github.com>
2026-09-15 11:03:26 +0200

    Add Contains-Duplicate (hash-table)

---
+++ leetcode/hash-table/217-Contains-Duplicate.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++){
+
+            if (map.containsKey(nums[i])){
+                return true;
+            }
+
+            map.put(nums[i] , map.getOrDefault(nums[i] , 0) + 1);
+
+        }
+
+        return false;
+        
+    }
+}
