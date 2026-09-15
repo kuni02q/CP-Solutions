fdee055044eb992a6e51669de2d36f7532fae5ec
kuni02q <70653128+kuni02q@users.noreply.github.com>
2026-09-15 11:14:56 +0200

    Add Valid-Anagram (hash-table)

---
+++ leetcode/hash-table/242-Valid-Anagram.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        Map<Character, Integer> map = new HashMap<>();
+        Map<Character, Integer> map2 = new HashMap<>();
+
+        if(s.length() != t.length()) return false;
+
+        for(int i = 0; i < s.length(); i++){
+
+            map.put(s.charAt(i) , map.getOrDefault(s.charAt(i) , 0) + 1);
+            map2.put(t.charAt(i) , map2.getOrDefault(t.charAt(i) , 0) + 1);
+
+        }
+
+        return map.equals(map2);
+
+        
+    }
+}
