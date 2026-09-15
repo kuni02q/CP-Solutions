64bed3277c502ae8afa5c8d1c05b4ff4c5feb2fd
kuni02q <70653128+kuni02q@users.noreply.github.com>
2026-09-15 12:02:09 +0200

    Add Is-Subsequence (dynamic-programming)

---
+++ leetcode/dynamic-programming/392-Is-Subsequence.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+
+        if (s.length() == 0)
+            return true;
+
+        int sIndex = 0;
+        for (int i = 0; i < t.length(); i++) {
+
+            if (s.charAt(sIndex) == t.charAt(i)) {
+                sIndex++;
+
+                if (sIndex == s.length())
+                    return true;
+            }
+
+        }
+
+        return false;
+
+    }
+}
